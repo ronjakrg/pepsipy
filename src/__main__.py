e123c2d4579aed972a3d985aff108560fb12a7f3
ronjakrg <ronja.krueger@student.hpi.de>
2025-04-30 21:37:57 +0200

    updated tests

--- src/__main__.py
+++ src/__main__.py
@@ -9,12 +9,12 @@
 import pandas as pd
 
 from src.features.sequence import aa_number, aa_frequency
-from src.plots.sequence import aa_distribution
+from src.plots.sequence import aa_freq_distribution
 
 peptides = pd.read_csv("./data/peptides.csv")
 peptides["aa_number"] = peptides["Sequence"].apply(aa_number)
 peptides["aa_frequency"] = peptides["Sequence"].apply(aa_frequency)
 #print(peptides.head())
 
-fig = aa_distribution(aa_frequency("PEPTIDE"))
+fig = aa_freq_distribution(aa_frequency("PEPTIDE"))
 fig.show()
--- src/features/sequence.py
+++ src/features/sequence.py
@@ -4,7 +4,11 @@ def aa_number(seq: str) -> int:
     """
     Computes the number of amino acids in a given sequence.
     """
-    return len(seq)
+    num = 0
+    for aa in seq:
+        if aa in AA:
+            num += 1
+    return num
 
 def aa_frequency(seq: str) -> dict[str, int]:
     """

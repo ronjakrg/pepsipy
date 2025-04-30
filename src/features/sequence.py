a33fd7b0377b11bf0987a885eaf5697cb2102dc2
ronjakrg <ronja.krueger@student.hpi.de>
2025-04-30 21:28:34 +0200

    added method and plot for aa frequency

--- src/features/sequence.py
+++ src/features/sequence.py
@@ -1,5 +1,16 @@
+from src.utils import AA
+
 def aa_number(seq: str) -> int:
-    '''
+    """
     Computes the number of amino acids in a given sequence.
-    '''
+    """
     return len(seq)
+
+def aa_frequency(seq: str) -> dict[str, int]:
+    """
+    Computes the frequency of each amino acid in a given sequence.
+    """
+    freq = {val: 0 for val in AA}
+    for aa in seq:
+        freq[aa] += 1
+    return freq

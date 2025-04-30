8a9c703e364fb93d422b2ffeca6e8dc777ee3339
ronjakrg <ronja.krueger@student.hpi.de>
2025-04-30 19:40:15 +0200

    added main as sandbox code section

---
+++ src/__main__.py
@@ -0,0 +1,11 @@
+# ────────────────────────────────────────────────
+#                     PLAYGROUND
+#   This file is just an experimental sandbox
+#   and is not part of the implementation.
+#
+#   To run this code, execute python -m src.
+# ────────────────────────────────────────────────
+
+from src.features.sequence import aa_number
+
+print(aa_number("PEPTID"))

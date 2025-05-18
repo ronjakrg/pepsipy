5fb5dca858c4086b0b3075ee2a4325fb96c6250d
ronjakrg <ronja.krueger@student.hpi.de>
2025-05-18 18:43:46 +0200

    added test for get_groups

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,10 @@
-from peptidefeatures.utils import sanitize_sequence
+from peptidefeatures.utils import sanitize_sequence, get_group
 
 def test_sanitize_sequence():
     assert "PEPTIDE" == sanitize_sequence("pEPtiDe :)")
+
+
+def test_get_group():
+    groups = ["AD", "CTR"]
+    assert "AD" == get_group("AD01_C1_INSOLUBLE_01", groups)
+    assert "None" == get_group(":)", groups)

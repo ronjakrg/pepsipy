1c6d2bed5f883b9942b05b82e14112ce9eb50358
ronjakrg <ronja.krueger@student.hpi.de>
2025-05-18 18:40:22 +0200

    add box plot to compare features across groups

--- src/peptidefeatures/plots/compare.py
+++ src/peptidefeatures/plots/compare.py
@@ -6,28 +6,45 @@ from peptidefeatures.constants import COLORS
 from peptidefeatures.utils import get_group
 
 
-def features(
+def scatter_features(
     df: pd.DataFrame,
-    plot_type: str,
     groups: list,
     feature_a: str,
     feature_b: str,
 ) -> go.Figure:
     """
-    Creates a plot to compare two features across groups.
+    Creates a scatter plot to compare two features across groups.
     """
     peptides = df.copy()
     peptides["Group"] = peptides["Sample"].apply(lambda x: get_group(x, groups))
 
-    if plot_type == "scatter":
-        fig = px.scatter(
-            peptides,
-            x=feature_a,
-            y=feature_b,
-            color="Group",
-            color_discrete_sequence=COLORS,
-            title="Comparison of peptide features across groups",
-        )
-    elif plot_type == "histogramm":
-        print("Not implemented yet.")
+    fig = px.scatter(
+        peptides,
+        x=feature_a,
+        y=feature_b,
+        color="Group",
+        color_discrete_sequence=COLORS,
+        title="Comparison of Peptide Features Across Groups",
+    )
+    return fig
+
+def box_feature(
+    df: pd.DataFrame,
+    groups: list,
+    feature: str,
+) -> go.Figure:
+    """
+    Creates a box plot to compare a feature between groups.
+    """
+    peptides = df.copy()
+    peptides["Group"] = peptides["Sample"].apply(lambda x: get_group(x, groups))
+
+    fig = px.box(
+        peptides,
+        x="Group",
+        y=feature,
+        color="Group",
+        color_discrete_sequence=COLORS,
+        title=f"Distribution of {feature} Across Groups",
+    )
     return fig

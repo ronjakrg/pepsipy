70cf36e29d627e5d902bfd54992adf6be9542a65
ronjakrg <ronja.krueger@student.hpi.de>
2025-05-26 19:35:09 +0200

    changed statement order in molecular_weight method

--- src/peptidefeatures/features.py
+++ src/peptidefeatures/features.py
@@ -31,8 +31,8 @@ def molecular_weight(seq: str) -> float:
     Computes the average molecular weight of a given sequence in Da.
     Note: The input sequence must be pre-sanitized to compute only valid amino acids.
     """
+    num = aa_number(seq)
     try:
-        num = aa_number(seq)
         weight = sum(AA_WEIGHTS[aa] for aa in seq) - (num - 1) * WATER
         return round(weight, 3)
     except KeyError as e:

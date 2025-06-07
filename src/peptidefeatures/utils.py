f10289defb7fa7314863a3e2362064250ac51986
ronjakrg <ronja.krueger@student.hpi.de>
2025-06-07 16:55:31 +0200

    added method to get sequence column name

--- src/peptidefeatures/utils.py
+++ src/peptidefeatures/utils.py
@@ -20,15 +20,22 @@ def get_group(name: str, groups: list) -> str:
     return next((g for g in groups if name.startswith(g)), "None")
 
 
+def get_seq_column_name(df: pd.DataFrame) -> str:
+    """
+    Finds the first column of a dataframe that contains the substring "sequence".
+    """
+    seq_col_name = next((col for col in df.columns if "sequence" in col.lower()), None)
+    if seq_col_name == None:
+        raise ValueError(
+            f"None of the containing columns are recognized as sequence column: {df.columns}"
+        )
+    return seq_col_name
+
+
 def get_distinct_seq(df: pd.DataFrame) -> pd.DataFrame:
     """
     Finds the first column containing "sequence" in its header and
     returns a dataframe containing only unique sequences.
     """
-    seq_col_name = next((col for col in df.columns if "seq" in col.lower()), None)
-    if seq_col_name == None:
-        raise ValueError(
-            f"None of the containing columns are recognized as sequence column: {df.columns}"
-        )
-
+    seq_col_name = get_seq_column_name(df)
     return df[[seq_col_name]].drop_duplicates(keep="first")
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,21 @@
+import pytest
+
 import pandas as pd
 
-from peptidefeatures.utils import sanitize_seq, get_group, get_distinct_seq
+from peptidefeatures.utils import sanitize_seq, get_group, get_seq_column_name, get_distinct_seq
 
+data = {
+        "Sample": [
+            "AD01_C1_INSOLUBLE_01",
+            "CTR01_C1_INSOLUBLE_01",
+            "CTR01_C1_INSOLUBLE_01",
+        ],
+        "Protein ID": ["A0A075B6S2", "A0A075B6R2", "A0A075B6R2"],
+        "Sequence": ["FSGVPDR", "VTISVDK", "VTISVDK"],
+        "Intensity": [936840, 33411000, 33411000],
+        "PEP": [0.0068633, 0.057623, 0.057623],
+    }
+df = pd.DataFrame(data)
 
 def test_sanitize_seq():
     assert "PEPTIDE" == sanitize_seq("pEPtiDe :)")
@@ -13,19 +27,14 @@ def test_get_group():
     assert "None" == get_group(":)", groups)
 
 
+def test_get_seq_column_name():
+    assert "Sequence" == get_seq_column_name(df)
+    with pytest.raises(ValueError) as e:
+        get_seq_column_name(pd.DataFrame())
+    assert "None of the containing columns are recognized" in str(e.value)
+
+
 def test_get_distinct_seq():
-    data = {
-        "Sample": [
-            "AD01_C1_INSOLUBLE_01",
-            "CTR01_C1_INSOLUBLE_01",
-            "CTR01_C1_INSOLUBLE_01",
-        ],
-        "Protein ID": ["A0A075B6S2", "A0A075B6R2", "A0A075B6R2"],
-        "Sequence": ["FSGVPDR", "VTISVDK", "VTISVDK"],
-        "Intensity": [936840, 33411000, 33411000],
-        "PEP": [0.0068633, 0.057623, 0.057623],
-    }
-    df = pd.DataFrame(data)
     assert get_distinct_seq(df).equals(
         pd.DataFrame({"Sequence": ["FSGVPDR", "VTISVDK"]})
     )

e233f916d1ad93554b0deeb244a9c7a54e619f72
ronjakrg <ronja.krueger@student.hpi.de>
2025-06-13 15:24:16 +0200

    added sorting by classes in subplots for aa distribution plots

--- src/peptidefeatures/plots/other.py
+++ src/peptidefeatures/plots/other.py
@@ -1,8 +1,17 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
-from peptidefeatures.constants import AA_WEIGHTS, COLORS, HYDROPATHY_INDICES
+from peptidefeatures.constants import (
+    AA_WEIGHTS,
+    COLORS,
+    HYDROPATHY_INDICES,
+    CHEMICAL_CLASS,
+    CHARGE_CLASS,
+    CHEMICAL_CLASS_PER_AA,
+    CHARGE_CLASS_PER_AA,
+)
 from peptidefeatures.features import aa_frequency, aa_classification
 
 
@@ -14,40 +23,93 @@ def aa_distribution(
     """
     Computes a bar plot showing the frequency distribution for a given sequence.
         seq: Given sequence
-        order: Specification of how the amino acids should be sorted, can be any of "frequency", "alphabetical", "classification", "hydropathy" or "weight".
+        order: Specification of how the amino acids should be sorted, can be any of "frequency", "alphabetical", "classes chemical", "classes charge", "hydropathy" or "weight".
         show_all: Specification if all amino acids should be listed, even when not found in the sequence
     """
     freq = aa_frequency(seq)
     if not show_all:
         freq = {key: val for key, val in freq.items() if val > 0}
-    fig = px.bar(
-        x=list(freq.keys()),
-        y=list(freq.values()),
-        labels={
-            "x": "Amino Acid",
-            "y": "Frequency",
-        },
-        title=f"Amino Acid Frequency of Sequence {seq}",
-        color_discrete_sequence=COLORS,
-    )
-    fig.update_yaxes(tickmode="linear", tick0=0, dtick=1)
 
-    # Sort amino acids by given order specification
-    if order_by == "frequency":
-        fig.update_xaxes(categoryorder="total ascending")
-    elif order_by == "alphabetical":
-        fig.update_xaxes(categoryorder="category ascending")
-    elif order_by == "classification":
-        # TODO Add when aa classification is implemented
-        raise NotImplementedError
-    elif order_by == "hydropathy":
-        sorted_aa = sorted(list(freq.keys()), key=lambda aa: HYDROPATHY_INDICES[aa])
-        fig.update_xaxes(categoryorder="array", categoryarray=sorted_aa)
-    elif order_by == "weight":
-        sorted_aa = sorted(list(freq.keys()), key=lambda aa: AA_WEIGHTS[aa])
-        fig.update_xaxes(categoryorder="array", categoryarray=sorted_aa)
+    if order_by in ["frequency", "alphabetical", "hydropathy", "weight"]:
+        fig = px.bar(
+            x=list(freq.keys()),
+            y=list(freq.values()),
+            labels={
+                "x": "Amino Acid",
+                "y": "Frequency",
+            },
+            color_discrete_sequence=COLORS,
+        )
+        if order_by == "frequency":
+            fig.update_xaxes(categoryorder="total ascending")
+        elif order_by == "alphabetical":
+            fig.update_xaxes(categoryorder="category ascending")
+        elif order_by == "hydropathy":
+            sorted_aa = sorted(list(freq.keys()), key=lambda aa: HYDROPATHY_INDICES[aa])
+            fig.update_xaxes(categoryorder="array", categoryarray=sorted_aa)
+        elif order_by == "weight":
+            sorted_aa = sorted(list(freq.keys()), key=lambda aa: AA_WEIGHTS[aa])
+            fig.update_xaxes(categoryorder="array", categoryarray=sorted_aa)
+
+    elif order_by in ["classes chemical", "classes charge"]:
+        # Prepare dataframe
+        df = pd.DataFrame(
+            [
+                {
+                    "Amino Acid": aa,
+                    "Frequency": freq,
+                }
+                for aa, freq in freq.items()
+            ]
+        )
+        if order_by == "classes chemical":
+            df["Class"] = df["Amino Acid"].map(CHEMICAL_CLASS_PER_AA)
+            classes = list(CHEMICAL_CLASS.keys())
+        elif order_by == "classes charge":
+            df["Class"] = df["Amino Acid"].map(CHARGE_CLASS_PER_AA)
+            classes = list(CHARGE_CLASS.keys())
+
+        # Number of bars per column in subplots
+        col_widths = []
+        for cls in classes:
+            num_bars = df[df["Class"] == cls].shape[0]
+            col_widths.append(num_bars)
+        rel_column_widths = [w / sum(col_widths) for w in col_widths]
+
+        # Create plot
+        fig = make_subplots(
+            cols=len(classes),
+            rows=1,
+            subplot_titles=classes,
+            column_widths=rel_column_widths,
+            horizontal_spacing=0.3 / len(classes),
+            shared_yaxes=True,
+        )
+        # Create bar per class
+        CLASS_TO_COLOR = dict(zip(classes, COLORS))
+        # TODO Sort amino acids per class alphabetically
+        for i, cls in enumerate(classes):
+            sub_df = df[df["Class"] == cls]
+            fig.add_trace(
+                go.Bar(
+                    x=sub_df["Amino Acid"],
+                    y=sub_df["Frequency"],
+                    marker_color=CLASS_TO_COLOR[cls],
+                    showlegend=False,
+                ),
+                row=1,
+                col=i + 1,
+            )
+            fig.update_xaxes(title_text=None, row=1, col=i + 1)
+        fig.update_layout(
+            barmode="group",
+            yaxis_title="Frequency",
+        )
+
     else:
         raise ValueError(f"Unknown option for sorting amino acids: {order_by}.")
+    fig.update_layout(title=f"Amino Acid Frequency of Sequence {seq}")
+    fig.update_yaxes(tickmode="linear", tick0=0, dtick=1)
     return fig
 
 

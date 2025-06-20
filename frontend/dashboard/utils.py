2b7123dd62627e4bfebea3d11588959738586f8f
ronjakrg <ronja.krueger@student.hpi.de>
2025-06-20 11:54:47 +0200

    added docstring to views utils

--- frontend/dashboard/utils.py
+++ frontend/dashboard/utils.py
@@ -2,30 +2,39 @@ import pandas as pd
 from pathlib import Path
 from django.conf import settings
 
-from .forms import FORM_TO_FUNCTION
-
 
 def load_data(name: str) -> pd.DataFrame:
+    """
+    TODO
+    """
     data_path = Path(settings.PROJECT_DIR) / "data" / name
     return pd.read_csv(data_path)
 
 
-def get_params(forms: list) -> dict:
+def get_params(forms: list, mapping: dict) -> dict:
+    """
+    TODO
+    """
     result = {}
     for form in forms:
         if form.is_valid():
             if form.cleaned_data["select"]:
+                # Get params from form
                 params = {
                     key: val
                     for key, val in form.cleaned_data.items()
                     if key != "select"
                 }
-                params.update({FORM_TO_FUNCTION[type(form)]: True})
+                # Add function name as param for (un)selecting
+                params.update({mapping[type(form)]: True})
                 result.update(params)
     return result
 
 
 def get_features_for_seq(data: pd.DataFrame, seq: str) -> dict:
+    """
+    TODO
+    """
     matched = data[data["Sequence"] == seq]
     matched = matched.drop(
         columns=[

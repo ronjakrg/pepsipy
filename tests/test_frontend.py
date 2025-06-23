a61295e69cb08522e05ee144de325520cc4c7c76
ronjakrg <ronja.krueger@student.hpi.de>
2025-06-23 12:16:56 +0200

    added first frontend tests

---
+++ tests/test_frontend.py
@@ -0,0 +1,52 @@
+import pytest
+from django import forms
+
+from frontend.dashboard.forms import (
+    GeneralForm,
+    ThreeLetterCodeForm,
+    MolecularFormulaForm,
+    SeqLengthForm,
+    MolecularWeightForm,
+    GravyForm,
+    IsoelectricPointForm,
+    AromaticityForm,
+    AaDistributionForm,
+    HydropathyProfileForm,
+    ClassificationForm,
+    CompareFeaturesForm,
+    CompareFeatureForm,
+    FORM_TO_FEATURE_FUNCTION,
+    FORM_TO_PLOT_FUNCTION,
+)
+
+@pytest.mark.parametrize("data", [
+    {"data_name": "peptides.csv", "seq": "PEPTIDE"},
+])
+def test_forms_general(data):
+    form = GeneralForm(data=data)
+    assert form.is_valid()
+
+@pytest.mark.parametrize("FormClass", [
+    ThreeLetterCodeForm,
+    MolecularFormulaForm,
+    SeqLengthForm,
+    MolecularWeightForm,
+    GravyForm,
+    AromaticityForm,
+    HydropathyProfileForm,
+    ClassificationForm,
+])
+def test_forms_checkboxes(FormClass):
+    form = FormClass(data={})
+    assert form.is_valid()
+    assert form.cleaned_data["select"] is False
+    form = FormClass(data={"select": "on"})
+    assert form.is_valid()
+    assert form.cleaned_data["select"] is True
+
+def test_utils():
+    pass
+
+
+def test_views():
+    pass

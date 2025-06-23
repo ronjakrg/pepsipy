2031307098cc4abb96700a338eda5b41fdc538db
ronjakrg <ronja.krueger@student.hpi.de>
2025-06-23 13:48:20 +0200

    formatted code

--- frontend/dashboard/test_dashboard.py
+++ frontend/dashboard/test_dashboard.py
@@ -18,23 +18,31 @@ from .forms import (
     FORM_TO_PLOT_FUNCTION,
 )
 
-@pytest.mark.parametrize("data", [
-    {"data_name": "peptides.csv", "seq": "PEPTIDE"},
-])
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {"data_name": "peptides.csv", "seq": "PEPTIDE"},
+    ],
+)
 def test_forms_general(data):
     form = GeneralForm(data=data)
     assert form.is_valid()
 
+
 # Testing forms that only contain checkboxes
-@pytest.mark.parametrize("FormClass", [
-    ThreeLetterCodeForm,
-    MolecularFormulaForm,
-    SeqLengthForm,
-    MolecularWeightForm,
-    GravyForm,
-    AromaticityForm,
-    HydropathyProfileForm,
-])
+@pytest.mark.parametrize(
+    "FormClass",
+    [
+        ThreeLetterCodeForm,
+        MolecularFormulaForm,
+        SeqLengthForm,
+        MolecularWeightForm,
+        GravyForm,
+        AromaticityForm,
+        HydropathyProfileForm,
+    ],
+)
 def test_forms_checkboxes(FormClass):
     form = FormClass(data={})
     assert form.is_valid()
@@ -43,17 +51,22 @@ def test_forms_checkboxes(FormClass):
     assert form.is_valid()
     assert form.cleaned_data["select"] is True
 
-@pytest.mark.parametrize("data", [
-    {
-        "select": "on",
-        "isoelectric_point_option": "bjellqvist",
-    }
-])
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        {
+            "select": "on",
+            "isoelectric_point_option": "bjellqvist",
+        }
+    ],
+)
 def test_isoelectric_point_form(data):
     form = IsoelectricPointForm(data=data)
     assert form.is_valid()
     assert form.cleaned_data["select"] is True
     assert form.cleaned_data["isoelectric_point_option"] == "bjellqvist"
+
 
 def test_utils():
     pass

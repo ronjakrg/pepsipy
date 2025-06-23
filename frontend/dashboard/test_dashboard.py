f28766afbf955480c03d16675f1b4aa12b7f3c95
ronjakrg <ronja.krueger@student.hpi.de>
2025-06-23 14:23:41 +0200

    added coverage dependency, removed forms.py from coverage report

--- frontend/dashboard/test_dashboard.py
+++ frontend/dashboard/test_dashboard.py
@@ -1,89 +1,23 @@
 import pytest
-
-from frontend.dashboard.forms import (
-    GeneralForm,
-    ThreeLetterCodeForm,
-    MolecularFormulaForm,
-    SeqLengthForm,
-    MolecularWeightForm,
-    GravyForm,
-    IsoelectricPointForm,
-    AromaticityForm,
-    HydropathyProfileForm,
-    AaDistributionForm,
+from frontend.dashboard.utils import (
+    load_data,
+    get_params,
+    get_features_for_seq,
 )
+from frontend.dashboard.views import overview
 
 
-@pytest.mark.parametrize(
-    "data",
-    [
-        {"data_name": "peptides.csv", "seq": "PEPTIDE"},
-    ],
-)
-def test_forms_general(data):
-    form = GeneralForm(data=data)
-    assert form.is_valid()
-
-
-# Testing forms that only contain checkboxes
-@pytest.mark.parametrize(
-    "FormClass",
-    [
-        ThreeLetterCodeForm,
-        MolecularFormulaForm,
-        SeqLengthForm,
-        MolecularWeightForm,
-        GravyForm,
-        AromaticityForm,
-        HydropathyProfileForm,
-    ],
-)
-def test_forms_checkboxes(FormClass):
-    form = FormClass(data={})
-    assert form.is_valid()
-    assert not form.cleaned_data["select"]
-    form = FormClass(data={"select": "on"})
-    assert form.is_valid()
-    assert form.cleaned_data["select"]
-
-
-@pytest.mark.parametrize(
-    "data",
-    [
-        {
-            "select": "on",
-            "isoelectric_point_option": "bjellqvist",
-        }
-    ],
-)
-def test_isoelectric_point_form(data):
-    form = IsoelectricPointForm(data=data)
-    assert form.is_valid()
-    assert form.cleaned_data["select"]
-    assert form.cleaned_data["isoelectric_point_option"] == "bjellqvist"
-
-
-@pytest.mark.parametrize(
-    "data",
-    [
-        {
-            "select": "on",
-            "aa_distribution_order_by": "frequency",
-            "aa_distribution_show_all": True,
-        }
-    ],
-)
-def test_aa_distribution_form(data):
-    form = AaDistributionForm(data=data)
-    assert form.is_valid()
-    assert form.cleaned_data["select"]
-    assert form.cleaned_data["aa_distribution_order_by"] == "frequency"
-    assert form.cleaned_data["aa_distribution_show_all"]
-
-
-def test_utils():
+def test_load_data():
     pass
 
 
-def test_views():
+def test_get_params():
+    pass
+
+
+def test_get_features_for_seq():
+    pass
+
+
+def test_overview():
     pass

e740fbfd71803aa0c4906730d2d7660e66df72d5
ronjakrg <ronja.krueger@student.hpi.de>
2025-07-02 09:28:29 +0200

    exposed calculator class for importing

--- src/pepsi/__main__.py
+++ src/pepsi/__main__.py
@@ -5,26 +5,3 @@
 #
 #   To run this code, execute python -m peptidefeatures.
 # ──────────────────────────────────────────────────────
-
-import pandas as pd
-from pepsi.calculator import Calculator
-
-print("# Executing main code from peptidefeatures ... #")
-
-calc = Calculator()
-calc.set_dataset(
-    pd.read_csv("/home/ronja/git/thesis-peptide-features/data/peptides.csv")
-)
-calc.set_feature_params(
-    gravy=True,
-)
-calc.set_plot_params(
-    aa_distribution=True,
-    aa_distribution_show_all=True,
-)
-calc.set_seq("NVHWYQQKPGQAPVLVIYR")
-result = calc.get_features()
-plots = calc.get_plots()
-
-plot = calc.aa_distribution("PEPTIDE", "classes chemical", True)
-plot.show()

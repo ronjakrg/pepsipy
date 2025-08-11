5e07b14623ea739d108026be8b3be88fc3b4f325
ronjakrg <ronja.krueger@student.hpi.de>
2025-08-11 20:16:05 +0200

    added tests for set methods

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -14,3 +14,19 @@ def test_init():
     assert METADATA.equals(calc.metadata)
     assert list(METADATA.columns) == calc.metadata_list
     assert METADATA.columns[0] == calc.key_metadata
+
+
+def test_set_feature_params():
+    calc = Calculator()
+    calc.set_feature_params(gravy=True)
+    assert ("gravy", True) in calc.feature_params.items()
+    assert ("molecular_weight", False) in calc.feature_params.items()
+    assert "self" not in calc.feature_params.keys()
+
+
+def test_set_feature_params():
+    calc = Calculator()
+    calc.set_plot_params(hydropathy_profile=True)
+    assert ("hydropathy_profile", True) in calc.plot_params.items()
+    assert ("titration_curve", False) in calc.plot_params.items()
+    assert "self" not in calc.plot_params.keys()
